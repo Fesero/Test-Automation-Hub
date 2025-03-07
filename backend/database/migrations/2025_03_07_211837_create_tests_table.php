4cf23d0fa7f19a979c67c26f7b396db8011dd4fe
Fesero <fesero@bk.ru>
2025-03-08 00:33:55 +0300

    backend

---
+++ backend/database/migrations/2025_03_07_211837_create_tests_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tests', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name'); // Название теста
+            $table->text('description')->nullable(); // Описание теста
+            $table->enum('status', ['pending', 'running', 'completed', 'failed'])->default('pending'); // Статус теста
+            $table->float('execution_time')->nullable(); // Время выполнения
+            $table->json('result')->nullable(); // Результаты выполнения JSON
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tests');
+    }
+};
